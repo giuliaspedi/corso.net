685af818e19ad0df625d3d60d48d27f11517598a
giuliaspedi <giuliaspedicato@gmail.com>
2019-02-04 15:50:24 +0100

    secondo commint

---
+++ Esercizi.Net/esercizi.Net.BusinessLogic/ITransazione.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizi.Net.BusinessLogic
+{
+    public interface ITransazione
+    {
+        string Tipo { get; set; }
+        string Categoria { get; set; }
+        string Descrizione { get; set; }
+        DateTime DataTransazione { get; set; }
+        decimal Importo { get; set; }
+    }
+
+}
